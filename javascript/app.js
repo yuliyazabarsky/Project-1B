f7088035cfa2b5fe4d06fc208af0963835ba03b7
yuliyazabarsky <yuliya1975@msn.com>
2019-03-19 19:26:45 -0600

    added modals on city / state input

--- javascript/app.js
+++ javascript/app.js
@@ -3,42 +3,97 @@ var brewery = []
 $("#runSearch").on("click", function (event) {
     event.preventDefault();
 
+
     var city = $("#city-input").val();
     var state = $("#state-input").val();
+    var modal = 0;
 
     // validateForm
-    var x = $("#state-input").val();
-    if (x == "") {
-        alert("State must be selected");
+    var y = $("#city-input").val();
+    if (y == "") {
+        modal = document.getElementById('myCityModal');
+        modal.style.display = "block";
+        // alert("Please enter city name");
+        // var modal = document.getElementById('myModal');
+
+        // // Get the button that opens the modal
+        // var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        // btn.onclick = function () {
+        //     modal.style.display = "block";
+        // }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
         return false;
     }
 
-        var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state + "&per_page=50";
-        // var queryURL = "https://api.openbrewerydb.org/breweries?by_postal_code=" + zip + "&per_page=30";
-        // Creates AJAX call for the specific movie button being clicked
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            for (var i = 0; i < response.length; i++) {
-                // console.log(response[i]);
-                if (!response[i].latitude || !response[i].longitude) {
-                    i++;
-                } else {
-                    brewery.push(response[i]);
-                }
+    var x = $("#state-input").val();
+
+    if (x == "") {
+        modal = document.getElementById('myStateModal');
+        modal.style.display = "block";
+        var span = document.getElementsByClassName("close")[1];
+
+        // When the user clicks the button, open the modal 
+        // btn.onclick = function () {
+        //     modal.style.display = "block";
+        // }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
             }
-            map(brewery);
-        });
+        }
 
-        
-        $("#city-input").val("");
-        $("#state-input").val("");
-       
+        return false;
+    }
+
+    var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city + "&by_state=" + state + "&per_page=50";
+    // var queryURL = "https://api.openbrewerydb.org/breweries?by_postal_code=" + zip + "&per_page=30";
+    // Creates AJAX call for the specific movie button being clicked
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        for (var i = 0; i < response.length; i++) {
+            // console.log(response[i]);
+            if (!response[i].latitude || !response[i].longitude) {
+                i++;
+            } else {
+                brewery.push(response[i]);
+            }
+        }
+        map(brewery);
+    });
+
+
+    $("#city-input").val("");
+    $("#state-input").val("");
 
 
 
-    
+
+
 
 
 });
@@ -46,7 +101,7 @@ $("#runSearch").on("click", function (event) {
 console.log(brewery);
 
 function map(cords) {
-    
+
     var map = L.map('map').setView([cords[1].latitude, cords[1].longitude], 10);
     mapLink =
         '<a href="http://openstreetmap.org">OpenStreetMap</a>';
@@ -74,26 +129,26 @@ function map(cords) {
 
 //     console.log();
 //     console.log("------------------------------------");
-  
+
 //     // Loop through and build elements for the defined number of articles
 //     for (var i = 0; i < numArticles; i++) {
 //       // Get specific article info for current index
 //       var article = NYTData.response.docs[i];
-  
+
 //       // Increase the articleCount (track article # - starting at 1)
 //       var articleCount = i + 1;
-  
+
 //       // Create the  list group to contain the articles and add the article content for each
 //       var $articleList = $("<ul>");
 //       $articleList.addClass("list-group");
-  
+
 //       // Add the newly created element to the DOM
 //       $("#article-section").append($articleList);
-  
+
 //       // If the article has a headline, log and append to $articleList
 //       var headline = article.headline;
 //       var $articleListItem = $("<li class='list-group-item articleHeadline'>");
-  
+
 //       if (headline && headline.main) {
 //         console.log(headline.main);
 //         $articleListItem.append(
@@ -105,4 +160,3 @@ function map(cords) {
 //             "</strong>"
 //         );
 //       }
-  
